808eb04e37d7de4282600d67398bae1cef42065c
Filipe Fernandes <ocefpaf@gmail.com>
2019-02-22 18:10:59 -0300

    fix wheel building

--- setup.py
+++ setup.py
@@ -1,27 +1,27 @@
-import io
-import os
+from pathlib import Path
 
 from setuptools import find_packages, setup
 
 import versioneer
 
-rootpath = os.path.abspath(os.path.dirname(__file__))
+rootpath = Path(__file__).parent.absolute()
 
 
 def read(*parts):
-    with open(os.path.join(rootpath, *parts), "rb") as f:
-        return f.read().decode("utf-8")
+    return open(rootpath.joinpath(*parts), "r").read()
 
 
-with io.open("requirements.txt") as f:
+with open("requirements.txt") as f:
     requires = f.readlines()
 install_requires = [req.strip() for req in requires]
 
 
 setup(
     name="erddapy",
+    python_requires='>=3.6',
     version=versioneer.get_version(),
     description="Python interface for ERDDAP",
+    license="BSD-3-Clause",
     long_description=f'{read("README.md")}',
     long_description_content_type="text/markdown",
     author="Filipe Fernandes",
@@ -32,9 +32,13 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "Topic :: Scientific/Engineering :: GIS",
         "License :: OSI Approved :: BSD License",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
     ],
+    platforms="any",
     packages=find_packages(),
     extras_require={"testing": ["pytest"]},
     install_requires=install_requires,
