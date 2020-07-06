e751396b4db5fc22b8ac1dd68916c989058e653d
Filipe Fernandes <ocefpaf@gmail.com>
2020-07-06 16:53:08 -0300

    add show iframe

--- erddapy/utilities.py
+++ erddapy/utilities.py
@@ -127,3 +127,10 @@ def _tempnc(data: BinaryIO) -> Generator[str, None, None]:
     finally:
         if tmp is not None:
             tmp.close()
+
+
+def show_iframe(src):
+    """Helper function to show HTML returns."""
+    from IPython.display import IFrame
+
+    return IFrame(src=src, width="100%", height=950)
