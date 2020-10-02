58bb28b0e2eeeeb7b8c9f137a50a83aed3d36d79
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-10-02 15:34:15 +0000

    Update docs after building Travis build 343 of ioos/erddapy
    The docs were built from the branch 'master' against the commit
    3da37a8f75a4b5a85bb132c64694154aace5b18e.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/ioos/erddapy/jobs/732287544.
    
    The doctr command that was run is
    
        /home/travis/micromamba/envs/TEST/lib/python3.8/site-packages/doctr/__main__.py deploy --build-tags --key-path github_deploy_key_ioos_erddapy.enc --built-docs docs/build/html dev

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.6.1.dev2+g9333ab0',
+    VERSION: '0.7.2.dev2+g3da37a8',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
