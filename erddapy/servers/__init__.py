59291fd7f55aa88939e4b073db4bac12fab959b3
Vini Salazar <17276653+vinisalazar@users.noreply.github.com>
2022-08-01 11:23:14 -0300

    Make servers object available from init module
      This allows importing the servers dict with 'from erddapy.servers import servers'

--- erddapy/servers/__init__.py
+++ erddapy/servers/__init__.py
@@ -4,3 +4,9 @@ Servers subpackage for the erddapy parent package.
 Contains the servers.py module and the erddaps.json file,
 which is a fallback when the awesome-erddap URL cannot be reached.
 """
+
+from erddapy.servers.servers import servers
+
+__all__ = [
+    "servers",
+]
