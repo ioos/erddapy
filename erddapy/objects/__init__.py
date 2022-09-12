19e38d850c8bce5d8bb01baae8b396be99167f7e
Vini Salazar <17276653+vinisalazar@users.noreply.github.com>
2022-09-12 18:29:12 -0300

    Drafting new classes
      This commit introduces a rough draft of the classes to be introduced in the
      object/opinionated layer. For now, the subpackage containing them is named 'objects'
      but that may change in the future. For discussion, see #228.
    
      - Add 'objects' subpackage with init module
      - Add 'objects.py' module with 5 new classes
        - Methods will be implemented in the following commits.

---
+++ erddapy/objects/__init__.py
@@ -0,0 +1,23 @@
+"""
+This module contains opinionated, higher-level objects for searching servers and accessing datasets.
+
+It is named 'objects' after object-relational mapping, which is the concept of having an object-oriented
+layer between a database (in this case, ERDDAP), and the programming language.
+"""
+
+
+from objects import (
+    ERDDAPConnection,
+    ERDDAPDataset,
+    ERDDAPServer,
+    GridDataset,
+    TableDataset,
+)
+
+__all__ = [
+    "ERDDAPDataset",
+    "ERDDAPConnection",
+    "ERDDAPServer",
+    "TableDataset",
+    "GridDataset",
+]
