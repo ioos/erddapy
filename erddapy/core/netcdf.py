0ea49e23c54feccc377a5ccb37753d8c4173e7cb
Vini Salazar <17276653+vinisalazar@users.noreply.github.com>
2022-10-04 11:35:58 +1100

    Add delete=False flag in _tempnc
      - Fix from bravoserver/bravo#111
      - This error is further documented on Stack Overflow (see #261 for discussion)

--- erddapy/core/netcdf.py
+++ erddapy/core/netcdf.py
@@ -30,7 +30,7 @@ def _tempnc(data: BinaryIO) -> Generator[str, None, None]:
 
     tmp = None
     try:
-        tmp = NamedTemporaryFile(suffix=".nc", prefix="erddapy_")
+        tmp = NamedTemporaryFile(suffix=".nc", prefix="erddapy_", delete=False)
         tmp.write(data.read())
         tmp.flush()
         yield tmp.name
