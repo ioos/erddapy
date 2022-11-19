3ed7d61057fa7231ac8c96b39de6f9bcae3dd667
Vini Salazar <17276653+vinisalazar@users.noreply.github.com>
2022-11-19 20:12:05 +1100

    Add public methods to erddapy module
      - Add public methods to erddapy.__init__
    
      This allows using the URL parsing and interfaces methods directly from the erddapy module,
      without having to access the core.url and core.interfaces modules.

--- erddapy/__init__.py
+++ erddapy/__init__.py
@@ -1,9 +1,31 @@
 """Easier access to scientific data."""
 
+from erddapy.core.interfaces import to_iris, to_ncCF, to_pandas, to_xarray
+from erddapy.core.url import (
+    get_categorize_url,
+    get_download_url,
+    get_info_url,
+    get_search_url,
+    parse_dates,
+    urlopen,
+)
 from erddapy.erddapy import ERDDAP
 from erddapy.servers.servers import servers
 
-__all__ = ["ERDDAP", "servers"]
+__all__ = [
+    "ERDDAP",
+    "servers",
+    "get_categorize_url",
+    "get_download_url",
+    "get_info_url",
+    "get_search_url",
+    "parse_dates",
+    "urlopen",
+    "to_ncCF",
+    "to_iris",
+    "to_pandas",
+    "to_xarray",
+]
 
 try:
     from ._version import __version__
