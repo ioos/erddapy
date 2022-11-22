6fdb280abfc8dccce9f1f1c043723add642b33e6
Vini Salazar <17276653+vinisalazar@users.noreply.github.com>
2022-11-22 11:38:06 +1100

    Expose methods in erddapy.core
      - Expose methods in erddapy.core instead of erddapy
      - Code review for #281

--- erddapy/__init__.py
+++ erddapy/__init__.py
@@ -1,30 +1,11 @@
 """Easier access to scientific data."""
 
-from erddapy.core.interfaces import to_iris, to_ncCF, to_pandas, to_xarray
-from erddapy.core.url import (
-    get_categorize_url,
-    get_download_url,
-    get_info_url,
-    get_search_url,
-    parse_dates,
-    urlopen,
-)
 from erddapy.erddapy import ERDDAP
 from erddapy.servers.servers import servers
 
 __all__ = [
     "ERDDAP",
     "servers",
-    "get_categorize_url",
-    "get_download_url",
-    "get_info_url",
-    "get_search_url",
-    "parse_dates",
-    "urlopen",
-    "to_ncCF",
-    "to_iris",
-    "to_pandas",
-    "to_xarray",
 ]
 
 try:
--- erddapy/core/__init__.py
+++ erddapy/core/__init__.py
@@ -3,3 +3,24 @@ Core subpackage for the erddapy parent package.
 
 This package contains the URL and data handling functionalities.
 """
+
+from erddapy.core.interfaces import to_iris, to_ncCF, to_pandas, to_xarray
+from erddapy.core.url import (
+    get_categorize_url,
+    get_download_url,
+    get_info_url,
+    get_search_url,
+    parse_dates,
+)
+
+__all__ = [
+    "get_categorize_url",
+    "get_download_url",
+    "get_info_url",
+    "get_search_url",
+    "parse_dates",
+    "to_iris",
+    "to_ncCF",
+    "to_pandas",
+    "to_xarray",
+]
