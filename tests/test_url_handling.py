3a1f86fc8e82c4a967e4ab95c2295be87110e27e
Alex Kerney <abk@mac.com>
2022-12-06 21:34:06 -0500

    Remove the timeout test
    Remove the timeout test that was using a service killed by Heroku. We are really testing that httpx appropriately times out when a timeout is passed to it, and hopefully that is being tested upstream.
    
    Closes  #289

--- tests/test_url_handling.py
+++ tests/test_url_handling.py
@@ -27,18 +27,6 @@ def test_urlopen_raise():
 
 
 @pytest.mark.web
-def test_urlopen_requests_kwargs():
-    """Test that urlopen can pass kwargs to requests."""
-    base_url = "https://standards.sensors.ioos.us/erddap/tabledap/"
-    timeout_seconds = 1  # request timeout in seconds
-    slowwly_milliseconds = (timeout_seconds + 1) * 1000
-    slowwly_url = f"https://flash-the-slow-api.herokuapp.com/delay/{slowwly_milliseconds}/url/{base_url}"
-
-    with pytest.raises(httpx.ReadTimeout):
-        urlopen(slowwly_url, timeout=timeout_seconds)
-
-
-@pytest.mark.web
 @pytest.mark.vcr()
 def test_check_url_response():
     """Test if a bad request returns HTTPError."""
